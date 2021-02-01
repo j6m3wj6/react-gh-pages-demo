db5134f55ea6d04ffcea13beb158272beeba0272
Tong <b05502072@ntu.edu.tw>
2021-02-01 15:31:05 +0800

    append 8 products

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,8 @@ import {
   Divider,
   Grid,
   Form,
-  List
+  List,
+  Header
 } from 'semantic-ui-react'
 
 
@@ -20,21 +21,21 @@ const BiddingInfo = (props) => {
     setInfo(newInfo);
   }
   const handleSubmit = async () => {
-    let completeInfo = true;
-    Object.values(info).forEach(element => {
-      if (element.length == 0) completeInfo=false;
-    });
-    const updateData = {
-        _no: props._no,
-        updateContent: {
-            bidding: {
-                ...info,
-                time: Date.now()
-            }
-        }
-    }
-    if (completeInfo) submitBidding(updateData)
-    else console.log("not complete")
+    // let completeInfo = true;
+    // Object.values(info).forEach(element => {
+    //   if (element.length == 0) completeInfo=false;
+    // });
+    // const updateData = {
+    //     _no: props._no,
+    //     updateContent: {
+    //         bidding: {
+    //             ...info,
+    //             time: Date.now()
+    //         }
+    //     }
+    // }
+    // if (completeInfo) submitBidding(updateData)
+    // else console.log("not complete")
   }
   const submitBidding = async (data) => {
     return await axios.post('https://tymphany-bidding-server.herokuapp.com/api/bidding', 
@@ -54,6 +55,9 @@ const BiddingInfo = (props) => {
       <Form.Field>
         <label id='movie'>姓名 (Name)</label>
         <Form.Input
+          required={true}
+          // error={'error'}
+          
           placeholder='Tong Wang'
           name='name'
           onChange={handleChange}
@@ -62,6 +66,8 @@ const BiddingInfo = (props) => {
       <Form.Field>
         <label>工號 (EmployeeID)</label>
         <Form.Input
+          required={true}
+          pattern="[0-9]{7}"
           placeholder='2000xxx'
           name='number'
           onChange={handleChange}
@@ -70,6 +76,8 @@ const BiddingInfo = (props) => {
       <Form.Field>
         <label>Email </label>
         <Form.Input
+          required={true}
+          type='email'
           placeholder='tong.wang@tymphany.com'
           name='email'
           onChange={handleChange}
@@ -78,6 +86,8 @@ const BiddingInfo = (props) => {
       <Form.Field>
         <label>出價 (Bidding Price) </label>
         <Form.Input
+          required={true}
+          pattern="([50]|[00]{2})\w+"
           placeholder='tong.wang@tymphany.com'
           name='price'
           onChange={handleChange}
@@ -97,42 +107,26 @@ const BiddingInfo = (props) => {
 }
 
 const DetailInfo = (props) => {
-  const [data, setData] = React.useState({})
-  const [showContent, setContent] = React.useState(false)
-  const fetchResource = async() => {
-    const res = await axios.get(
-      `https://tymphany-bidding-server.herokuapp.com/api/bidding?_no=${props._no}`
-    )
-    // const res = await axios.get(
-    //   `http://localhost:5000/api/bidding?_no=${props._no}`
-    // )
-    console.log('fetchResource price', res.data.content[0].bidding)
-    setData(res.data.content[0].bidding)
-    
-    res.data.content[0].bidding.sort(function(a, b) {
-      return (b.price - a.price)
-    });
 
-    setContent(true)
-  }
-  useEffect(() => { 
-    fetchResource();
-  }, [])
-  
   return (
     <>
-      <List divided relaxed className='detail'>
-        {data.length > 0 && data.map((_d, index) => {
+      <div className='detail'>
+        <span className='conditions'><strong> Check: </strong>{props.data.conditions}</span>
+        <p className='shortage'><strong> Shortage: </strong> {props.data.shortage}</p>
+      </div>
+      <Divider horizontal>History</Divider>
+      <List divided relaxed className='bidding'>
+        {props.biddingData.length > 0 && props.biddingData.map((_d, index) => {
           return(
             <List.Item>
               <List.Content floated='right'>
-                  <h5 className='detail-name'>${_d.price}</h5>
+                  <h5 className='bidding-name'>${_d.price}</h5>
               </List.Content>
               <List.Icon name='dollar' size='large' verticalAlign='middle' />
               
               <List.Content>
-                  <h5 className='detail-name'>{_d.name}</h5>
-                  <p className='detail-time'>{moment(_d.time).format("MMMM Do YYYY, h:mm:ss a")}</p>
+                  <h5 className='bidding-name'>{_d.name}</h5>
+                  <p className='bidding-time'>{moment(_d.time).format("MMMM Do YYYY, h:mm:ss a")}</p>
                 
               </List.Content>
               
@@ -149,7 +143,7 @@ function Product(props) {
   const dataLength = props.data.length? props.data.length : 0;
   const [showDetail, setDetail] = React.useState(-1);
   const [showBid, setBid] = React.useState(-1);
-
+  
   const handleOnClickDetail = (event) => {
     // console.log(event.target.name)
     setDetail(event.target.name)
@@ -170,23 +164,32 @@ function Product(props) {
   }
 
   const Card = ({data, index} ) => {
+    const biddingData = data.bidding
+    biddingData.sort(function(a, b) {
+        return (b.price - a.price)
+    });
+    console.log(biddingData)
+    const heighestPrice = biddingData.length>0? `即時出價 $ ${biddingData[0].price}`: '尚未有人出價'
     return (
       <div className='tg_card' >
-        <Image className='img' centered src={process.env.PUBLIC_URL+ `/img/${data._no}.jpg`}/>
+        <div className='img-content'>
+          <Image className='img' centered src={process.env.PUBLIC_URL+ `/img/${data._no}.jpg`}/>
+        </div>
         <div style={{ height: 'fit-content' }}>
           <h5 className='brand'> {`#${data._no} ${data.module}`}{data._no}</h5>
           <p>{data.brand}</p>
+          {data.set.length>0 && <p className='set'>set with: {data.set}</p>}
         </div>
         <Divider />
         <div>
-          <p className='price-title'> 即時出價 $ </p>
+          <p className='price-title'> {heighestPrice}</p>
         </div>
         <div className='operations-buttons'>
           <Button inverted color='blue' className='operation-button' name={index} onClick={handleOnClickDetail}> Detail </Button>
           <Button inverted color='red' className='operation-button' name={index} onClick={handleOnClickBid}> Bid </Button>
           {(index == showDetail || index == showBid) && <Icon name="chevron up" bordered style={{minWidth: '30px', margin: 'auto', borderRadius: '3px'}} onClick={chevronUp}/>}
         </div>
-        {index == showDetail && <DetailInfo _no={data._no} />}
+        {index == showDetail && <DetailInfo data={data} biddingData={biddingData} />}
         {index == showBid && <BiddingInfo _no={data._no} submitFinish={submitFinish}/>}
       </div>   
     )
